1994bd9f4a5f7bc8dc5bebe2bbd2ce615935d57f
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-08-25 22:39:38 +0500

    use the link

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,11 @@
+import Navber from '@/compoents/Navber'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div>
+      <Navber/>
+    </div>
   )
 }
 
---
+++ my-app/src/compoents/Navber.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navber = () => {
+  return (
+    <div>
+        <Link href="/product">got yje link</Link>
+    </div>
+  )
+}
+
+export default Navber
