e031a3dcd667105ec8aba5b91d65b9fc00a999aa
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-08-26 15:37:46 +0500

    use the router hook

--- my-app/src/app/khan/[[...names]]/page.jsx
+++ my-app/src/app/khan/[[...names]]/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function names() {
   return (
-    <div>names</div>
+    <div>names
+      
+    </div>
   )
 }
 
--- my-app/src/compoents/Navber.jsx
+++ my-app/src/compoents/Navber.jsx
@@ -1,10 +1,17 @@
+
+'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navber = () => {
+  const router = useRouter()
+  const names = 2
   return (
     <div>
-        <Link href="/product">got yje link</Link>
+        <Link href="/user/setting">got yje link</Link>
+        <br />
+        <button  onClick={()=> router.push(`/user/profile/${names}`)}>go to profile</button>
     </div>
   )
 }
