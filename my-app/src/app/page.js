34b2fd34569ec1f85166379f21f3ead32f3ea232
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-08-26 22:01:00 +0500

    use the image

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,17 @@
+// import Image from '../compoents/Image.jsx'
 import Navber from '@/compoents/Navber'
 import React from 'react'
+import Image from 'next/image'
+import my from "../../public/image/m.jpeg"
+
 
 function page() {
   return (
     <div>
-      <Navber/>
+      {/* <Navber/> */}
+      {/* <Image/> */}
+
+      <Image src={my} width={200} height={200}/>
     </div>
   )
 }
