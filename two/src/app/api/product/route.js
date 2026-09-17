2a7afa6afc4a206071e719cfdcf3b9b43b07ba10
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-09-17 14:33:23 +0500

    how to use the headrs

--- two/src/app/api/product/route.js
+++ two/src/app/api/product/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET (req, res) { 
+      const head = new Headers(req.headers)
+      console.log("head",head.get("content-type"))
+
    return NextResponse.json({
     msg:"hello this mu hammad "
    })
