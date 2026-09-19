d4d44f74ad21e6f84c587d94806b232e30eeb536
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-09-19 14:56:27 +0500

    how to use the dynmic route

---
+++ two/src/app/api/product/[id]/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+
+export const GET = async (req,context )=>{
+
+
+    const ali= await context.params
+    console.log(ali.id)
+
+    return NextResponse.json({
+        msg: "hello this is the product with id"
+    })
+}
--- two/src/app/api/product/route.js
+++ two/src/app/api/product/route.js
@@ -2,9 +2,21 @@ import { NextResponse } from "next/server";
 
 export async function GET (req, res) { 
       const head = new Headers(req.headers)
-      console.log("head",head.get("content-type"))
+    //   console.log("head",head.get("content-type"))
+
+    const { searchParams } = new URL(req.url)
+    console.log(searchParams)
 
    return NextResponse.json({
     msg:"hello this mu hammad "
    })
+}
+export async function POST (req){
+    const body = await req.json()
+    console.log(body)
+    console.log("hi")
+    return NextResponse.json({
+        msg:"hello this mu hammad ",
+        body:body
+       })
 }
